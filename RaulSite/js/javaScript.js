e693a5359bb0d3010dd3d027ee287b6dd4e19215
Raul Diaz <dudadew311@gmail.com>
2017-05-01 00:04:06 -0400

    Initial commit. Still a long way to go.

---
+++ RaulSite/js/javaScript.js
@@ -0,0 +1,42 @@
+function calculate_age(birth_month,birth_day,birth_year)
+{
+    today_date = new Date();
+    today_year = today_date.getFullYear();
+    today_month = today_date.getMonth();
+    today_day = today_date.getDate();
+    age = today_year - birth_year;
+
+    if ( today_month < (birth_month - 1))
+    {
+        age--;
+    }
+    if (((birth_month - 1) == today_month) && (today_day < birth_day))
+    {
+        age--;
+    }
+    return age;
+}
+
+$(document).ready(function(){
+	$('#age').html(calculate_age(11,12,1976));
+	$(".pics").delay(2000).animate({right:'262px'});
+	
+});
+
+$('#dispStats').click(function() {
+	$('#stats').toggle(1000);
+	var dis = $('#dispStats').html();
+	if (dis == "Show Stats"){
+		$('#dispStats').html("Hide Stats");
+	} else
+		$('#dispStats').html("Show Stats");
+});
+
+$('#raulDiv').click(function() {
+    $('#ironDiv').fadeIn(500);
+});
+
+$('#ironDiv').click(function() {
+	$('#ironDiv').fadeOut(500);
+});
+
